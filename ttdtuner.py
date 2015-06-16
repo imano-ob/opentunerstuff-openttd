b3772fa65c4bada91ac7579c095175af3509ca19
Renan T. Carneiro <renanteruoc@gmail.com>
2015-06-16 18:10:29 -0300

    Tuner placeholder adaptado pro handler novo

--- ttdtuner.py
+++ ttdtuner.py
@@ -6,47 +6,60 @@ import opentuner
 from opentuner import MeasurementInterface
 from opentuner import ConfigurationManipulator
 from opentuner import IntegerParameter
+from opentuner import SwitchParameter
 from opentuner import Result
 
 import time
+import random
 #import subprocess
-#import threading
+import threading
 #import argparser
 
-import ttdtest
+import ttdhandler
 
-class TTDTest(MeasurementInterface):
+class TTDTuner(MeasurementInterface):
 
     def __init__(self, *pargs, **kwargs):
-        super(TTDTest, self).__init__(*pargs, **kwargs)
+        super(TTDTuner, self).__init__(*pargs, **kwargs)
         self.parallel_compile = True
-    
+        self.handler = ttdhandler.TTDHandler()
+        self.handler.start()
+        time.sleep(3)
+
+        #Might be useful in the future, who knows
+        #It already has been
+        self.dbglock = threading.Lock()
+        
     def manipulator(self):
         manipulator = ConfigurationManipulator()
-        manipulator.add_parameter(IntegerParameter('POINTLESS', 0, 100))
-        self.handler = ttdtest.TTDHandler()
-        self.handler.start()
-        time.sleep(1)
+        
+        manipulator.add_parameter(SwitchParameter('AI', 3))
+
+        self.set_ais()
+        
         return manipulator
 
-    def compile(self, cfg, od):
-        print "aeho"
-        self.handler.add_ai('derp')
-        time.sleep(100)
-        return 10
+    def set_ais(self):
+        self.ais = ['AIAI', 'Convoy', 'CivilAI']
     
+    def compile(self, cfg, result_id):
+        self.handler.add_ai(self.ais[cfg['AI']], result_id)
+        return 0
+    
+    def run_precompiled(self, desired_result, input, limit, compile_result,
+                        result_id):
+        res = self.handler.result(result_id)
+        return Result(time = -res)
+        
+    def run(self, desired_result, input, limit):
+        pass
+
     def save_final_config(self, configuration):
         self.handler.shutdown()
 
-    def run_precompiled(self, desired_result, input, limit, compile_result,
-                        result_id):
-        return Result(time = 10)
-        
-    def run(self, desired_result, input, limit):
- #       print "derasd"
-        pass
-        
+
+    
 if __name__ == '__main__':
   argparser = opentuner.default_argparser()
   args = argparser.parse_args()
-  TTDTest.main(args)
+  TTDTuner.main(args)
