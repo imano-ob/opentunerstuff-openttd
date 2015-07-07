8d3bd9a8bd25baaaf33285322cb93fa76e442702
Renan T. Carneiro <renanteruoc@gmail.com>
2015-07-07 15:38:16 -0300

    Parser versão porca implementada. Assume que AIs vão ter um único output da forma [ID][Resultado] Closes #3, #4 e #7

--- ttdhandler.py
+++ ttdhandler.py
@@ -1,4 +1,6 @@
 
+# -*- coding: UTF-8 -*-
+
 import subprocess
 import threading
 import sys
@@ -41,37 +43,43 @@ class TTDHandler():
         #Recarrega AIs para incluir a AI recem gerada
         self.server.stdin.write("reload_ai")
         self.server.stdin.write("start_ai {}\n".format(ai))
-        #TODO: pegar id na ai no openttd?
         self.server_in_lock.release()
 
-    def stop_ai(self, ai_id):
+    def stop_ai(self, ttd_id):
         self.server_in_lock.acquire()   
-        #self.server.stdin.write("stop_ai {}\n".format(ai))
-        #TODO: Stop AI
+        self.server.stdin.write("stop_ai {}\n".format(ttd_id + 1)) #reasons
         self.server_in_lock.release()
         self.ai_sem.release()
         
     def read_output(self):
         while True:
             last_line = self.server.stdout.readline()
-            ai_id, content = self.parse(last_line)
-            #TODO: arranjar um jeito de lidar com o comando companies do Openttd
+            ai_id, ttd_id, content = self.parse(last_line)
             if ai_id == None:
                 continue
             self.server_out_locks[ai_id].acquire()
-            #...huh. acho que isso pode nao fazer sentido
-            #depende se a ai no final vai cuspir mais de uma mensagem?
             self.bufs[ai_id] = content
+            self.stop_ai(ttd_id)
             self.server_out_locks[ai_id].release()
             self.server_in_locks[ai_id].release()
 
     def parse(self, line):
-        #TODO: parsear
         #AIs: começam com [script]
-        #...huh
         #São da forma [script][<ID>][<Error/Warning/Info>] <Texto>
-        #Deve dar pra usar isso pra extrair o ID in TTD?
-        pass
+        #AIs devem ter output da froma [<Tuner ID>][<Resultado>] for
+        #simplicity's sake
+        
+        tmp = line.split(']')
+        for field in tmp:
+            field.strip('[')
+        if tmp != 'script':
+            return None, None, None
+        #Campos restantes são irrelevantes para nossos propósitos
+        ai_id = tmp[3]
+        ttd_id = tmp[1]
+        content = tmp[4]
+
+        return ai_id, ttd_id, content
             
     def result(self, ai_id):
         #temp codez
