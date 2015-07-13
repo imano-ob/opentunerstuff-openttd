4f7640426fae0960c71e2bd6e531a76ff65dcef8
Renan T. Carneiro <renanteruoc@gmail.com>
2015-07-13 08:59:01 -0300

    Extraídas todas as escritas pro servidor em um método Also, limpeza de prints de debug que acho que não são necessárias no momento

--- ttdhandler.py
+++ ttdhandler.py
@@ -9,13 +9,10 @@ import time
 class TTDHandler():
     def __init__(self,
                  #Maximo 15
-                 max_ais = 8,
                  ais_per_round = 8):
-        self.max_ais = max_ais
         #Not currently used
         self.ais_per_round = ais_per_round
 
-        self.ai_sem = threading.Semaphore(self.max_ais)
         self.server_in_lock = threading.Lock()
         self.server_out_locks = {}
         self.server_in_locks = {}
@@ -33,42 +30,25 @@ class TTDHandler():
                 
     def add_ai(self, ai, ai_id):
 
-        print "uno"
-        
-        self.ai_sem.acquire()
-
-        print "dos"
         #management stuff
         self.server_out_locks[ai_id] = threading.Lock()
         self.server_in_locks[ai_id] = threading.Lock()
 
         #server communication
-        self.server_in_lock.acquire()
-        #Recarrega AIs para incluir a AI recem gerada
-        cmd = "rescan_ai\n"
-        print cmd
-        self.server.stdin.write(cmd)
-        cmd = "start_ai {}\n".format(ai)
-        print cmd
-        self.server.stdin.write(cmd)
-
-        print "tres"
+        cmd = "rescan_ai"
+        self.write_to_server(cmd)
+        cmd = "start_ai {}".format(ai)
+        self.write_to_server(cmd)
         
-        self.server_in_lock.release()
-
     def stop_ai(self, ttd_id):
-        self.server_in_lock.acquire()   
-        self.server.stdin.write("stop_ai {}\n".format(ttd_id + 1)) #reasons
-        self.server_in_lock.release()
-        self.ai_sem.release()
+        cmd = "stop_ai {}".format(ttd_id + 1) #reasons
+        self.write_to_server(cmd)
         
     def read_output(self):
-        print "asdasdasd"
         while True:
             last_line = self.server.stdout.readline()
             print last_line
             if last_line == '':
-                print "ohnoes"
                 break
             ai_id, ttd_id, content = self.parse(last_line)
             if ai_id == None:
@@ -84,12 +64,10 @@ class TTDHandler():
         #São da forma [script][<ID>][<Error/Warning/Info>] <Texto>
         #AIs devem ter output da froma [<Tuner ID>][<Resultado>] for
         #simplicity's sake
-        print "in parse"
         tmp = line.split(']')
         for field in tmp:
             field.strip('[')
         if tmp != 'script':
-            print "pointless"
             return None, None, None
         #Campos restantes são irrelevantes para nossos propósitos
         #TODO: Possivelmente certificar que vai continuar funcionando
@@ -104,7 +82,7 @@ class TTDHandler():
     def result(self, ai_id):
         #temp codez
         print "Heyoooooooooooooooooooo"
-        time.sleep(10)
+        time.sleep(100)
         return 10
         #real codez
         #TODO: cleanup de coisas relacionadas à ai que vai morrer
@@ -116,7 +94,13 @@ class TTDHandler():
         
     def shutdown(self):
         #TODO: cleanup
-        self.server_in_lock.acquire()   
-        self.server.stdin.write("quit\n")
-        self.server_in_lock.release()
+        self.write_to_server("quit")
 
+    def write_to_server(self, msg):
+        sleep_time = 0.5
+        #Coderproofing
+        msg += '\n'
+        self.server_in_lock.acquire()
+        self.server.stdin.write(msg)
+        time.sleep(sleep_time)
+        self.server_in_lock.release()
