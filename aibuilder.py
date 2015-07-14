dd992b716954a2bcabe2c90f0558cc7293b5cb70
Renan T. Carneiro <renanteruoc@gmail.com>
2015-07-14 18:30:51 -0300

    AI Builder pre alpha 0.01

---
+++ aibuilder.py
@@ -0,0 +1,15 @@
+
+class AIBuilder:
+    def __init__(self):
+        self.base_ai_directory = None
+        self.destiny_ai_directory = None
+        self.base_header_file = None
+        self.base_footer_file = None
+        self.final_file = None
+    
+    def build(self, args):
+        #system.copy(from base, to ai dir, lotsastuff)
+        #system.cat(base header, stuff, footer) > final file
+        self.?????()
+        self.profit()
+        
