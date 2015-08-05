a4a07bdf905d8f41d2329b42d980b28c85a327c4
Renan T. Carneiro <renanteruoc@gmail.com>
2015-08-05 18:05:35 -0300

    Diretorios do builder lidos de um arquivo de config local. Also, progresso.

--- aibuilder.py
+++ aibuilder.py
@@ -1,20 +1,22 @@
 
 import shutil
+import config
 
 class AIBuilder:
     def __init__(self):
-        self.base_ai_directory = None #./base_ai
-        self.destiny_ai_directory = None #~/.openttd/ai
-        self.cost_file = None #base_directory/pathfinder.nut
-        self.info_file = None #base_directory/info.nut
-        self.main_file = None #base_directory/main.nut
+        self.base_dir = config.base_dir #./base_ai
+        self.target_dir = config.target_dir #~/.openttd/ai
+        #TODO: definir e/ou verificar coisas abaixo
+        self.cost_file = 'costs.nut' #base_directory/pathfinder.nut
+        self.info_file = 'info.nut' #base_directory/info.nut
+        self.main_file = 'main.nut' #base_directory/main.nut
         #Talvez limpeza?
         
     def build(self, args, ai_id):
         #Isso vai copiar inclusive coisas a serem reescritas depois
         #TODO: tratar erros
         try:
-            shutil.copytree(self.base_ai_directory, self.destiny_ai_directory)
+            shutil.copytree(self.base_dir, self.target_dir)
         except OSError as e:
             #Error 17: arquivo já existe
             #Não vamos tratar caso seja outro erro
@@ -24,22 +26,25 @@ class AIBuilder:
         #TODO: tratar args
         arg_string = self.build_arg_string(args)
         self.substitute(self.cost_file, arg_string)
+        
         #TODO: geram ai_name
         self.substitute(self.info_file, ai_name)
         self.substitute(self.main_file, ai_name)
         
     def substitute(self, target_file, content):
-        #TODO: caminhos
-        in_file = open(base/target, 'r')
-        out_file = open(destiny/target, 'w')
-        while line = in_file.readline() != '':
+        base_path = self.base_dir + target_file
+        target_path = self.target_dir + target_file
+        base_file = open(base_path, 'r')
+        target_file = open(target_path, 'w')
+        line = base_file.readline()
+        while line != '':
             #TODO: arranjar uma string menos idiota
             if line.find("changeme") != -1 :
-                out_file.write(content + '\n')
+                target_file.write(content + '\n')
             else:
-                out_file.write(line)
-        in_file.close()
-        out_file.close()
+                target_file.write(line)
+        base_file.close()
+        target_file.close()
         
     def build_arg_string(self, args):
         #too lazy
