7c96e1746cab6e946bbc4121347da6814a18ab9a
Renan T. Carneiro <renanteruoc@gmail.com>
2015-08-25 17:01:22 -0300

    aibuilder menos bugadao

--- aibuilder.py
+++ aibuilder.py
@@ -1,4 +1,6 @@
 
+# -*- coding: UTF-8 -*-
+
 import shutil
 import config
 
@@ -15,43 +17,49 @@ class AIBuilder:
     def build(self, args, ai_id):
         #Isso vai copiar inclusive coisas a serem reescritas depois
         #TODO: tratar erros
+        ai_name = self.base_name + str(ai_id)
+        target_dir = self.target_dir + "/" + ai_name
+        print target_dir
         try:
-            shutil.copytree(self.base_dir, self.target_dir)
+            shutil.copytree(self.base_dir, target_dir)
         except OSError as e:
             #Error 17: arquivo já existe
             #Não vamos tratar caso seja outro erro
             if e.errno != 17:
                 raise
             #TODO: arranjar alguma coisa pra resolver esse caso
-            raise
+            shutil.rmtree(target_dir)
         #TODO: tratar args
         arg_string = self.build_arg_string(args)
         self.substitute(self.cost_file, arg_string)
         
-        ai_name = self.base_name + str(ai_id)
-        str = '	function GetName()        { return "{}"; }
-	function CreateInstance() { return "{}"; }'.format(ai_name, ai_name)
-        self.substitute(self.info_file, str)
-        str = 'AICompany.SetName("{}");'.format(ai_name)
-        self.substitute(self.main_file, str)
+        aux = '	function GetName() {return "%s"}\n\tfunction CreateInstance() {return "%s"}"; }' % (ai_name, ai_name)
+        self.substitute(self.info_file, aux)
+        aux = 'AICompany.SetName("{}");'.format(ai_name)
+        self.substitute(self.main_file, aux)
         
     def substitute(self, target_file, content):
-        base_path = self.base_dir + target_file
-        target_path = self.target_dir + target_file
+        base_path = self.base_dir + '/' + target_file
+        target_path = self.target_dir+ '/' + target_file
         base_file = open(base_path, 'r')
         target_file = open(target_path, 'w')
         line = base_file.readline()
         while line != '':
+            print line
             #TODO: arranjar uma string menos idiota
             if line.find("changeme") != -1 :
                 target_file.write(content + '\n')
             else:
                 target_file.write(line)
+            line = base_file.readline()
+        print ': )'
         base_file.close()
         target_file.close()
         
     def build_arg_string(self, args):
         str = ""
+        #temp
+        return ""
         str += "this._max_cost = {};".format(args['MAX_COST'])
         str += "this._cost_tile = {};".format(args['COST_TILE'])
 	str += "this._cost_diagonal_tile = {};".format(args['COST_DIAGONAL'])
