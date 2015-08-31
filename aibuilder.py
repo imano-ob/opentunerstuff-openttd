0531fd3d00d0ed428d28c9eb5d10b689d7d90b5d
Renan T. Carneiro <renanteruoc@gmail.com>
2015-08-31 16:23:21 -0300

    builder passa ai_id pra ai. Also, bugfixes

--- aibuilder.py
+++ aibuilder.py
@@ -18,25 +18,31 @@ class AIBuilder:
         #Isso vai copiar inclusive coisas a serem reescritas depois
         #TODO: tratar erros
         ai_name = self.base_name + str(ai_id)
-        target_dir = self.target_dir + "/" + ai_name
-        print target_dir
+        self.target_dir = self.target_dir + "/" + ai_name
+        print self.target_dir
         try:
-            shutil.copytree(self.base_dir, target_dir)
+            shutil.copytree(self.base_dir, self.target_dir)
+            print ': )'
         except OSError as e:
+            print ': ('
             #Error 17: arquivo já existe
             #Não vamos tratar caso seja outro erro
             if e.errno != 17:
                 raise
             #TODO: arranjar alguma coisa pra resolver esse caso
-            shutil.rmtree(target_dir)
+            shutil.rmtree(self.target_dir)
+            shutil.copytree(self.base_dir, self.target_dir)
         #TODO: tratar args
-        arg_string = self.build_arg_string(args)
-        self.substitute(self.cost_file, arg_string)
-        
-        aux = '	function GetName() {return "%s"}\n\tfunction CreateInstance() {return "%s"}"; }' % (ai_name, ai_name)
-        self.substitute(self.info_file, aux)
-        aux = 'AICompany.SetName("{}");'.format(ai_name)
-        self.substitute(self.main_file, aux)
+        finally:
+            arg_string = self.build_arg_string(args)
+            self.substitute(self.cost_file, arg_string)
+            
+            aux = '	function GetName() {return "%s"}\n' % (ai_name)
+            aux +=' function CreateInstance() {return "%s"}"; }\n' % (ai_name)
+            self.substitute(self.info_file, aux)
+            aux = 'AICompany.SetName("{}");\n'.format(ai_name)
+            aux += 'local ai_id = {};\n'.format(ai_id)
+            self.substitute(self.main_file, aux)
         
     def substitute(self, target_file, content):
         base_path = self.base_dir + '/' + target_file
@@ -44,22 +50,25 @@ class AIBuilder:
         base_file = open(base_path, 'r')
         target_file = open(target_path, 'w')
         line = base_file.readline()
+        print target_path
         while line != '':
-            print line
+#            print line
             #TODO: arranjar uma string menos idiota
             if line.find("changeme") != -1 :
+                print line
+                print content
                 target_file.write(content + '\n')
             else:
                 target_file.write(line)
             line = base_file.readline()
-        print ': )'
+#        print ': )'
         base_file.close()
         target_file.close()
         
     def build_arg_string(self, args):
         str = ""
         #temp
-#        return ""
+        return ""
         str += "this._max_cost = {};\n".format(args['MAX_COST'])
         str += "this._cost_tile = {};\n".format(args['COST_TILE'])
 	str += "this._cost_diagonal_tile = {};\n".format(args['COST_DIAGONAL'])
