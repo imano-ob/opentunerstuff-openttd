c066abcfcc82b81cc05982e0527a7a1d09aa5d30
Renan <renanteruoc@gmail.com>
2015-09-01 09:09:28 -0300

    fixes no aibuilder

--- aibuilder.py
+++ aibuilder.py
@@ -18,10 +18,10 @@ class AIBuilder:
         #Isso vai copiar inclusive coisas a serem reescritas depois
         #TODO: tratar erros
         ai_name = self.base_name + str(ai_id)
-        self.target_dir = self.target_dir + "/" + ai_name
-        print self.target_dir
+        target_dir = self.target_dir + "/" + ai_name + "/"
+        print target_dir
         try:
-            shutil.copytree(self.base_dir, self.target_dir)
+            shutil.copytree(self.base_dir, target_dir)
             print ': )'
         except OSError as e:
             print ': ('
@@ -30,23 +30,25 @@ class AIBuilder:
             if e.errno != 17:
                 raise
             #TODO: arranjar alguma coisa pra resolver esse caso
-            shutil.rmtree(self.target_dir)
-            shutil.copytree(self.base_dir, self.target_dir)
+            shutil.rmtree(target_dir)
+            shutil.copytree(self.base_dir, target_dir)
         #TODO: tratar args
         finally:
+
             arg_string = self.build_arg_string(args)
-            self.substitute(self.cost_file, arg_string)
+            self.substitute(target_dir, self.cost_file, arg_string)
             
             aux = '	function GetName() {return "%s";}\n' % (ai_name)
-            aux +=' function CreateInstance() {return "%s";}" }\n' % (ai_name)
-            self.substitute(self.info_file, aux)
+            aux +=' function CreateInstance() {return "%s";} \n' % (ai_name)
+            self.substitute(target_dir, self.info_file, aux)
+
             aux = 'AICompany.SetName("{}");\n'.format(ai_name)
             aux += 'local ai_id = {};\n'.format(ai_id)
-            self.substitute(self.main_file, aux)
+            self.substitute(target_dir, self.main_file, aux)
         
-    def substitute(self, target_file, content):
+    def substitute(self, target_dir, target_file, content):
         base_path = self.base_dir + '/' + target_file
-        target_path = self.target_dir+ '/' + target_file
+        target_path = target_dir+ '/' + target_file
         base_file = open(base_path, 'r')
         target_file = open(target_path, 'w')
         line = base_file.readline()
